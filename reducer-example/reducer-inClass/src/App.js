0e109f02297fcad2ac46cadf9b7e7e7e78983cdb
“red” <karatasmert255@gmail.com>
2024-01-07 16:14:31 +0300

    Add reducer example to project

---
+++ reducer-example/reducer-inClass/src/App.js
@@ -0,0 +1,11 @@
+import UseReducerExample from "./UserReducerExample";
+
+function App() {
+  return (
+    <div>
+      {/* <UseStateExample /> */}
+      <UseReducerExample />
+    </div>
+  );
+}
+export default App;
